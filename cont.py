bbd20967b0aaae984dba48af10aa4c92c82e3fc8
Jorge N <iajsenac@gmail.com>
2025-05-13 16:51:04 -0300

    inicial

---
+++ cont.py
@@ -0,0 +1,127 @@
+#Atv:39
+# from math import sqrt
+# #print(sqrt(9))
+
+# while True:
+#     num = int(input("Digite um número: "))
+#     if num < 0:
+#         print("Número inválido")
+#     elif num > 0:
+#         print(sqrt(num))
+#     else:
+#         print("Encerrado!")
+#         break
+
+
+#Atv:40
+# numero = 5
+# print("contando")
+# while True:
+#     print(numero)
+#     numero = numero - 1
+#     if numero == 0:
+#         break
+# print("Foi")
+
+#Atv:41
+# senha = int(input("Cadastre uma senha: "))
+
+# while True:
+#     tent = int(input("Digite a senha cadastrada: "))
+#     if tent == senha:
+#         print("Acesso concedido!")
+#         break
+#     else:
+#         print("Senha errada, tente novamente!")
+
+
+#Atv:42
+# tent = 0
+
+# while True:
+#     pin = int(input("Digite o pin: "))
+#     tent += 1
+#     if pin == 123456:
+#         print(f"PIN correto inserido, números de tentativas. {tent}")
+#         break
+#     else:
+#         print(tent)    
+
+
+#Atv:43
+# ano = int(input("Digite um ano:"))
+
+# while True:
+#     ano += 1
+#     if (ano % 4 == 0 and ano % 100 != 0) or (ano % 400 == 0):
+#         print(f"{ano} é um ano bissexto!")
+#         break
+        
+
+#Atv:44     
+# par = 0
+
+# while par < 30:
+#     par += 2
+#     print(par)
+    
+# print("finalizado")
+
+
+#Atv:45
+# print("Você está pronto?")
+# numero = int(input("Por favor, digite um número: "))
+
+# while numero == 0:
+#     print(numero)
+#     print("Agora")
+#     numero = int(input("Por favor, digite um número: "))
+
+
+# #Atv:46
+# var = 1
+# num = int(input("Digite um número: "))
+
+# while var < num:
+#     print(var)
+#     var += 1
+
+
+# #Atv:47
+# num = int(input("Digite um número: "))
+# dobro = 1
+
+# while dobro <= num:
+#     print(dobro)
+#     dobro *= 2
+# print(f"O limite é {num}")
+
+
+#Atv:48
+# num = int(input("Digite um número: "))
+# dobro = 1
+# base = int(input("Qual a base: "))
+
+# while dobro <= num:
+#     print(dobro)
+#     dobro *= base
+# print(f"O limite é {num}")
+
+# import re
+# import random
+
+# print(re.search("[A-Z]", "senhA"))
+
+# numero_secreto = random.randint(1, 200)
+# print(numero_secreto)
+
+
+#Atv:49-50
+# soma = 0
+# while soma <= 100:
+#     num = int(input("Digite um número: "))
+#     soma += num
+#     print(soma)
+    
+# print("limite")
+
