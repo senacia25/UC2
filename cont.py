86eccc6408c1f502145bb761abc10d008a1ff70f
Jorge N <iajsenac@gmail.com>
2025-05-14 16:59:28 -0300

    hoje

--- cont.py
+++ cont.py
@@ -96,7 +96,7 @@
 #     dobro *= 2
 # print(f"O limite é {num}")
 
-
+#refazer
 #Atv:48
 # num = int(input("Digite um número: "))
 # dobro = 1
@@ -124,4 +124,104 @@
 #     print(soma)
     
 # print("limite")
+
+
+# exem = "ele é o instrutor do curso"
+# total = len(exem)
+# print(total)
+
+           
+#Atv:51
+# import re
+
+#refazer
+# while True:
+#     senha = input("Digite a senha: ")
+#     lmai = re.search("[A-Z]", senha)
+#     lmin = re.search("[a-z]", senha) 
+#     tnum = re.search("[0-9]", senha)
+#     if len(senha) < 8:
+#         print("Senha precisa ter 8 cacteres.")
+    
+#     elif lmai == None:
+#         print("Precisa 1 letra maiúscula")
+#     elif lmin == None:
+#         print("1 letra minúscula")
+#     elif tnum == None:
+#         print("1 número")
+#     else:
+#         print("Senha compatível")
+#         break
+        
+
+# Atv:52
+# num = 152
+# while True:
+#     dig = int(input("Adivinhe o número: "))
+#     if dig > num:
+#         print("O número é menor")
+#     elif dig < num:
+#         print("o número é maior")
+#     else:
+#         print(f"{num} - Acertou")
+
+#outra forma atv:52
+# import random
+# numero_secreto = random.randint(1, 100)   
+# tent = 0
+# while True:
+#     num = int(input("Adivinhe o número: "))
+#     tent += 1
+    
+#     if numero_secreto < num :
+#         print("O número é menor")
+#     elif numero_secreto > num:
+#         print("o número é maior")
+#     else:
+#         print(f"{numero_secreto} - Acertou \n{tent} - Número de tentativas ")
+
+#refazer
+# #Atv:53
+# saldo = 500
+# print(f"Seu saldo é: {saldo}")
+# while True:
+#     saque = int(input("Digite um valor: "))
+        
+#     if saque % 10 != 0:
+#         print("O valor deve ser multilplicado por 10")
+#     elif saque > saldo:
+#         print("Saldo insuficiente")
+#     elif saque <= 0:
+#         print("Valor inválido")
+#     else:
+#         saldo -= saque
+#         print(saldo)
+#         print(saldo)
+        
+        
+ #Atv:54
+# while True:
+#     palavra1 = input("Digite uma palavra: ")   
+#     palavra2 = input("Digite outra palavra: ") 
+#     if len(palavra1) == len(palavra2):
+#          print("As palavras tem o mesmo número de caracterer")
+#          break
+#     else:
+#         print("Tamanhos diferente, tente novamente")
+
+#Atv:55
+# while True:
+#     palavra1 = input("Digite uma palavra: ")   
+#     palavra2 = input("Digite outra palavra: ") 
+#     if len(palavra1) > len(palavra2):
+#         print(f"{palavra1}Essa é maior")
+#     elif len(palavra1) == len(palavra2):
+#         print("As duas são do mesmo tamanho")
+#     else:
+#         print(f"{palavra2} É maior")
+    
+
+entrada = input("Digite um palavra: ")
+ultimo = len(entrada) - 1
+print(f"O último caracter da palavra é -{entrada[ultimo]}-")
 
